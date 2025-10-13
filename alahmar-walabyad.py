d2816d29b97ad82356dff71d3602c6dc8fcb8020
Zan-0xy <ahmadsan272@gmail.com>
2025-10-14 00:23:35 +0700

    Update alahmar-walabyad.py

--- alahmar-walabyad.py
+++ alahmar-walabyad.py
@@ -79,7 +79,6 @@ def countdown(t):
         if remaining_time > 0:
             stdout.flush() 
             stdout.write(f"\r{Fore.BLUE} [*] Attack status {Fore.YELLOW}=> {Fore.RED} {remaining_time:.2f} sec left {' ' * 26}")
-            print(f"{Fore.YELLOW}Target {Fore.GREEN}" +url+ " run}")
         else:
             stdout.flush()
             stdout.write(f"\r{Fore.MAGENTA} [*] Attack Done!{' ' * 53}|\n")
