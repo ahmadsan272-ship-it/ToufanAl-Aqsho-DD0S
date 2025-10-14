53807fcf4908268b652a872dc926d699af606af3
Zan-0xy <ahmadsan272@gmail.com>
2025-10-14 09:38:18 +0700

    Update T0ufanAl-Aqsha.py

--- T0ufanAl-Aqsha.py
+++ T0ufanAl-Aqsha.py
@@ -70,7 +70,7 @@ def get_user_input(prompt_message):
 # Fungsi Countdown untuk Menampilkan Waktu Serangan
 def countdown(t):
     until = datetime.date
-     time.now() + datetime.timedelta(seconds=int(t))
+    time.now() + datetime.timedelta(seconds=int(t))
     while True:
         remaining_time = (until - datetime.datetime.now()).total_seconds()
         if remaining_time > 0:
